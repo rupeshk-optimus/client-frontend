67c59fc05996aa48ad99c12abbdb09f9663af7a4
rupeshk-optimus <rupesh.kumar@optimusinfo.com>
2023-09-07 12:19:19 +0530

    first commit

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  serverBaseUrl: 'https://clientportalapistaging.azurewebsites.net/',
+  clientBaseUrl: 'https://clientportaluistaging.azurewebsites.net/',
+  employeePortalUrl: 'https://enkelportalstaging.azurewebsites.net/',
+  // tslint:disable-next-line: max-line-length
+  userGuideDocument: 'https://docs.google.com/document/d/e/2PACX-1vQf8FUNcZBLgOvYRxN35uBZ8I7xrcvSRDApRWVMF85WoQ-th6FTdZf-HCovP14EBs-n2_pCbu7rwoc_/pub?embedded=true',
+  appInsights: {
+      instrumentationKey: '35987410-1bd0-4d5a-87a5-af80162d09ce'
+  }
+};
---
+++ src/environments/environment.testing.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    serverBaseUrl: 'https://clientportalapitest.azurewebsites.net/',
+    clientBaseUrl: 'https://clientportaluitest.azurewebsites.net/',
+    employeePortalUrl: 'http://enkelportaluitest.azurewebsites.net/',
+    // tslint:disable-next-line: max-line-length
+    userGuideDocument: 'https://docs.google.com/document/d/e/2PACX-1vQf8FUNcZBLgOvYRxN35uBZ8I7xrcvSRDApRWVMF85WoQ-th6FTdZf-HCovP14EBs-n2_pCbu7rwoc_/pub?embedded=true',
+    appInsights: {
+        instrumentationKey: 'e27cb74a-8d24-4178-8629-83d47835fe2b'
+    }
+};
